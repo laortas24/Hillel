c726658ad0f4ced4a2a7365c78b355456f90e5ea
Roman Hatchenko <laortas24@gmail.com>
2021-05-18 12:27:20 +0300

    Create versions.tf

---
+++ Lesson01/versions.tf
@@ -0,0 +1,14 @@
+Dmitriy Pavlovskiy, [18 мая 2021 г., 12:24:37]:
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20"
+    }
+  }
+  required_version = ">= 0.14"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
